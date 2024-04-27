9dcd3c10aa071e9e55d8a6e271894069ba504729
abuhasanlikhon <168264035+abuhasanlikhon@users.noreply.github.com>
2024-04-27 14:30:49 +0600

    Add files via upload

---
+++ digitalclock.py
@@ -0,0 +1,16 @@
+import time
+from tkinter import*
+
+root=Tk()
+root.configure(background="black")
+
+def start():
+    text = time.strftime("%H:%M:%S")
+    Label.config(text=text)
+    Label.after(200,start)
+Label = Label(root,font=("DS-Digital",100),fg="red",bg="black")
+Label.grid(row=0, column=1)
+print("Done")
+start()
+root.mainloop()    
+
